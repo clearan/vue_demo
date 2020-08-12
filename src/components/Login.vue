85774377e1e036178cb7ba59eb450cfe99b75c74
clear <clearan77@gmail.com>
2020-08-12 19:09:28 +0800

    'login初步'

---
+++ src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="less" scoped>
+  .login_container {
+    background-color: #2d4d6d;
+    height: 100%;
+  }
+  .login_box {
+    width:450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+
+    .avatar_box {
+      height: 130px;
+      width: 130px;
+
+      img {
+        height: 130px;
+        width: 130px;
+
+      }
+    }
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '../components/Login'
 
 Vue.use(VueRouter)
 
-const routes = [
-]
-
 const router = new VueRouter({
-  routes
+  routes: [{ path: '/', redirect: '/login' },
+    { path: '/login', component: Login }]
 })
 
 export default router
